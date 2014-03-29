3ebf5b70b2e5c8f3eddf85e149d5a4889ed39066
Matt Freeman <rmf@rmfs-MacBook-Pro.local>
2014-03-29 11:30:18 -0700

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OauthDeskTrial::Application.config.secret_key_base = '01b5538843ffb7f1eb4e4fc041a5a2af671289f357d8358a062c7a225c28f053e5f5689dcc2726ef46225bc16e0f2d0f7e586356c559c48c87724738f5af8220'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OauthDeskTrial::Application.config.session_store :cookie_store, key: '_oauth_desk_trial_session'
