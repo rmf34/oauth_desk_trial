54ca079b463d1dd6eff5c8fd8d8ac6289c68034b
Matt Freeman <rmf@rmfs-MacBook-Pro.local>
2014-03-29 14:49:43 -0700

    Add omniauth and omniauth desk gems. Configure desk provider.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer unless Rails.env.production?
-  provider :desk, ENV['DESK_KEY'], ENV['DESK_SECRET']
+  provider :desk, ENV['DESK_KEY'], ENV['DESK_SECRET'], :site => 'https://rmf34.desk.com'
 end
