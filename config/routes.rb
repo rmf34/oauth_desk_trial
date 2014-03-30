3accb40d7ee61e8433163875dc3231a12d74e75c
Matt Freeman <rmf@rmfs-MacBook-Pro.local>
2014-03-30 11:47:40 -0700

    Refactor authentications routes and authentications new.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ OauthDeskTrial::Application.routes.draw do
 
   get '/auth/:provider/callback', to: 'authentications#create'
   resources :authentications
+  get '/auth/:provider/callback', :to => 'authentications#create'
+  resources :authentications, :only => [:new, :destroy] do
+    member do
+      get :desk_show
+    end
+  end
 
   get 'about', to: 'static_pages#about'
   root 'authentications#new'
